1abf1e1f286503c07764a04e1f8978fb5bf2ff2b
EduGese <eddugonz@gmail.com>
2023-05-23 13:47:54 +0200

    Quitado logica login logout de homepage

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-use App\Entity\User;
+
+
+
 use App\Entity\Activity;
 
 
@@ -16,30 +19,24 @@ use App\Entity\Activity;
          //Voy a usar este controlador para:
          //Crear actividad nueva
          //Editar una actividad creada
-         //Eliminar una actividad creada
+         //Eliminar una actividad creada   HECHO
 
 
 class ActivityController extends AbstractController
 {
-    #[Route('/activity', name: 'app_activity')]
-    public function index(): Response
-    {
-        return $this->render('activity/index.html.twig', [
-            'controller_name' => 'ActivityController',
-        ]);
-    }
+   
 
-    // public function activityListByUser(ManagerRegistry $doctrine, UserInterface $user): response{
-    // //    $user = new User();
-    //     $activity_repo = $doctrine->getManager()->getRepository(Activity::class);
-    //     $qb =  $activity_repo->createQueryBuilder('a')
-    //                         ->Where("a.id_user = :id_user")
-    //                         ->setParameter('id_user', $user->getId())
-    //                         ->orderBy('a.price', 'ASC')
-    //                         ->getQuery();
-    //     $resulSet = $qb->execute();
-    //     return $this->render('admin/adminActivitiesAll.html.twig',[
-    //         'activities' => $resulSet
-    //     ]);
-    // }
+    public function deleteActivity(ManagerRegistry $doctrine, UserInterface $user, Request $request): response{
+        
+        $idActivity = $request->attributes->get('id');
+        $activity_repo = $doctrine->getManager()->getRepository(Activity::class);
+        $activity = $activity_repo->find($idActivity);
+        $activity_manager = $doctrine->getManager();
+        $activity_manager->remove($activity);
+        $activity_manager->flush();
+
+        return $this->redirect(
+            $this->generateUrl('admin_dashboard')
+        );
+    }
 }
