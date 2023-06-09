227ad533d03980251bb45f8793be4e6776936c9f
EduGese <eddugonz@gmail.com>
2023-06-09 14:55:56 +0200

    Funcionalidad insertar review y score en actividad. En tabla user_activity

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Form\CreateActivityFormType;
+use App\Entity\User;
 
 use Symfony\Component\Security\Core\Security;
 
@@ -34,25 +35,27 @@ use App\Entity\Activity;
 class ActivityController extends AbstractController
 {
     private $entityManager;
+    private $doctrine;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(ManagerRegistry $doctrine, EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+        $this->doctrine = $doctrine;
     }
 
-    public function deleteActivity(Request $request, ActivityService $activityService,ManagerRegistry $doctrine,UserInterface $user,$id): Response {
+    public function deleteActivity(Request $request, ActivityService $activityService, ManagerRegistry $doctrine, UserInterface $user, $id): Response
+    {
         try {
             $id = $request->attributes->get('id');
             $activityService->deleteActivity($doctrine, $user, $id, $request);
-    
+
             // Mostrar mensaje de éxito
             $this->addFlash('success', sprintf('Successfully deleted activity with ID %d.', $id));
-    
         } catch (NotFoundHttpException $exception) {
             // Mostrar mensaje de error
             $this->addFlash('error', $exception->getMessage());
         }
-    
+
         return $this->redirectToRoute('show_admin_activities');
     }
     public function createActivity(Request $request, Security $security): response
@@ -96,12 +99,60 @@ class ActivityController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-    public function showActivityReviews( $id, ActivityService $activityService): response
+    public function showActivityReviews($id, ActivityService $activityService): response
     {
 
 
         $activityReviews = $activityService->getActivityReviews($id);
 
         return $this->render('showActivityReviews.html.twig', $activityReviews);
+    }
+    public function printActivityReviewForm(Request $request, ManagerRegistry $doctrine): response
+    {
+
+
+
+
+        $idActivity = $request->attributes->get('id');
+        $activity_repo = $doctrine->getRepository(Activity::class);
+        $activity = $activity_repo->find($idActivity);
+
+        return $this->render('activityReviewForm.html.twig', [
+            'activity' => $activity
+        ]);
+    }
+    public function addActivityReview(Request $request, ManagerRegistry $doctrine, EntityManagerInterface $entityManager, Security $security): response
+    {
+
+        $idActivity = $request->attributes->get('id'); //idactivity
+        $score = $request->request->get('score');
+        $review = $request->request->get('review');
+
+        $user = $security->getUser();
+        $user_repo = $doctrine->getRepository(User::class);
+        $user_id = $user_repo->find($user)->getId(); //id del usuario 
+
+        // Insertar o actualizar en la tabla user_activity
+        $conn = $entityManager->getConnection();
+        $query = 'INSERT INTO user_activity (user_id, activity_id, review, score)
+              VALUES (:user_id, :activity_id, :review, :score)
+              ON DUPLICATE KEY UPDATE review = VALUES(review), score = VALUES(score)';
+        $params = [
+            'user_id' => $user_id,
+            'activity_id' => $idActivity,
+            'review' => $review,
+            'score' => $score
+        ];
+        $conn->executeStatement($query, $params);
+
+
+
+        $idActivity = $request->attributes->get('id');
+        $activity_repo = $doctrine->getRepository(Activity::class);
+        $activity = $activity_repo->find($idActivity);
+
+        return $this->render('activityReviewForm.html.twig', [
+            'activity' => $activity
+        ]);
     }
 }
