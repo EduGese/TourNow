26e0a2dbf0599aa3319c014789d9f68e0b7d59a8
EduGese <eddugonz@gmail.com>
2023-06-12 19:47:10 +0200

    Cambiada zona horaria de la app

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// Carga las variables de entorno
+(new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+
+// Establece la zona horaria
+date_default_timezone_set('Europe/Madrid');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
