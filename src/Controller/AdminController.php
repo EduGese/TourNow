0b3836e818da16b2a06173ac7cc54ec2549f180a
EduGese <122921699+EduGese@users.noreply.github.com>
2023-08-24 17:22:10 +0200

    clean controllers, falta UserController part 2

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,11 +1,10 @@
 <?php 
-// src/Controller/Admincontroller.php
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+
 
 class AdminController extends AbstractController
 {
