d6d2222bab20d889d9617257e962c1afa3f32a18
EduGese <122921699+EduGese@users.noreply.github.com>
2023-08-24 16:47:43 +0200

    limpiar mapa2.js

--- public/scripts/mapa2.js
+++ public/scripts/mapa2.js
@@ -8,7 +8,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19
 }).addTo(mymap);
 
-//Obtener los elementos HTML que contienen las coordenadas
+
 var startUbication = document.getElementById('co_start').innerText;
 var startUbicationArray = startUbication.split(',');
 console.log(startUbicationArray[0]);
@@ -20,13 +20,13 @@ console.log(endUbicationArray[0]);
 console.log(endUbicationArray[1]);
 
 
-// Obtener las coordenadas de la ubicación inicial y final de la actividad
+
 var startCoord = [startUbicationArray[0],startUbicationArray[1]];
 var endCoord = [endUbicationArray[0], endUbicationArray[1]];
 
-// Agregar marcadores al mapa
+
 L.marker(startCoord).addTo(mymap);
 L.marker(endCoord).addTo(mymap);
 
-// Ajustar la vista del mapa para ver ambos marcadores
+
 mymap.fitBounds([startCoord, endCoord]);
